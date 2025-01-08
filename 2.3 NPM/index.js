805b0363bf3d8d6f5adcf5b6ce7e799d29c68804
Abhilash <137391776+AbiWD@users.noreply.github.com>
2025-01-08 15:35:10 +0530

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,21 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+
+
+
+// var gamerNamer = require('gamer-namer');
+
+// var name = gamerNamer.generateName();
+
+// console.log(`I am the mighty ${name}`);
+
+
+
+
+
+
+
+
